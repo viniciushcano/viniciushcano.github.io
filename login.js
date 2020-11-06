f5e39500b231203f5c31b2343c4744e9bb41e962
viniciushcano <vinicius.cano@bandtec.com.br>
2020-11-06 13:30:29 -0300

    New footer, index, login

---
+++ login.js
@@ -0,0 +1,9 @@
+function displayCadastro(){
+    document.getElementById('cadastro').style.display ='block';
+    document.getElementById('login').style.display = 'none';
+}
+function displayLogin(){
+    document.getElementById('cadastro').style.display ='none';
+    document.getElementById('login').style.display = 'block';
+}
+
